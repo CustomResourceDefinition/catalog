35d893545a57422aff2ef34c8a2a27f4df0f4246
Jakob Jensen <jakobj@jakobj.dk>
2024-06-06 18:54:27 +0200

    Only use docker to create shells (#9)

--- test/prepare-10-reset.sh
+++ test/prepare-10-reset.sh
@@ -1,11 +1,10 @@
 set -e
 
-printf "Reset for a fresh test ... "
+echo "Reset for a fresh test ... "
+
+helm repo ls -o json | jq -r '.[].name' | xargs -I {} helm repo rm {}
 
 rm -rf /schema/* &>/dev/null || true
-rm -rf /root/.cache/helm/* &>/dev/null || true
-rm -rf /root/.config/helm/* &>/dev/null || true
-rm -rf /root/.local/helm/* &>/dev/null || true
 rm -rf /templates/* &>/dev/null || true
 rm -rf /repository &>/dev/null || true
 rm -rf /verified-schema &>/dev/null || true
@@ -16,7 +15,7 @@ mkdir /repository/git
 mkdir /repository/http
 
 cd /repository/http
-killall -9 python3 &> /dev/null || true
+pgrep -f python3 | xargs -r kill -9 &> /dev/null || true
 (python3 -m http.server 2>&1 | grep -v '" 200 -' | grep -v 'Connection refused') &
 
 echo "done"
