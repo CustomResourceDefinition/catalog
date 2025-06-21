bfe10aaa1487e9fef3ce425aba6e7f3377adfb01
Jakob Jensen <jakobj@jakobj.dk>
2025-06-21 16:55:07 +0200

    Simpler makefile conditions

--- make.d/build.mk
+++ make.d/build.mk
@@ -1,20 +1,20 @@
 export RUNNER=ghcr.io/customresourcedefinition/catalog-runner:$(shell docker run -v $$(pwd)/Dockerfile:/Dockerfile --rm alpine /bin/sh -c 'md5sum < /Dockerfile | cut -f1 -d" "' 2>/dev/null)
 
 build:
-ifeq ($(strip $(CI)),)
-	test -n "$$(docker images -q $(RUNNER))" || \
-		docker build --tag $(RUNNER) .
-else ifeq ($(strip $(GITHUB_REF)),refs/heads/main)
+ifneq ($(strip $(CI)),)
+	echo "$$GITHUB_TOKEN" | docker login ghcr.io -u $$GITHUB_ACTOR --password-stdin
+endif
+
+ifeq ($(strip $(GITHUB_REF)),refs/heads/main)
 	test -n "$$(docker images -q $(RUNNER))" || \
 		docker pull $(RUNNER) || \
-		(echo "$$GITHUB_TOKEN" | docker login ghcr.io -u $$GITHUB_ACTOR --password-stdin && \
-		docker build --tag $(RUNNER) --push .)
+		docker build --tag $(RUNNER) --push .
 else
 	test -n "$$(docker images -q $(RUNNER))" || \
 		docker pull $(RUNNER) || \
-		(echo "$$GITHUB_TOKEN" | docker login ghcr.io -u $$GITHUB_ACTOR --password-stdin && \
-		docker build --tag $(RUNNER) .)
+		docker build --tag $(RUNNER) .
 endif
+
 	test -n "$$(docker images -q $(RUNNER))"
 	$(COMPOSE_RUN) \
 	runner make _build
