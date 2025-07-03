1588ff0e77f45df706b73c84e2672918b3cbf8ea
Jakob Jensen <jakobj@jakobj.dk>
2025-07-03 22:49:16 +0200

    tmp

---
+++ tools/git.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
+)
+
+func asdf() error {
+	// Destination to save the chart
+	tmpDir, err := os.MkdirTemp("", "git")
+	if err != nil {
+		return fmt.Errorf("failed to create temp dir: %w", err)
+	}
+
+	repo, err := git.PlainClone(tmpDir, &git.CloneOptions{
+		URL: "https://github.com/codereaper/lane",
+	})
+	if err != nil {
+		return err
+	}
+
+	iter, err := repo.Tags()
+	if err != nil {
+		return err
+	}
+
+	tags := make([]string, 0)
+	iter.ForEach(func(r *plumbing.Reference) error {
+		tags = append(tags, r.Name().Short())
+		return nil
+	})
+
+	return nil
+}
---
+++ tools/git_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// FIXME: more tests
+func TestGit(t *testing.T) {
+	err := asdf()
+	assert.Nil(t, err)
+}
