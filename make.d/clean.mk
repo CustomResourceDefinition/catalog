2895d79d472e53c7fa285511e95538e2a404f455
Jakob Jensen <jakobj@jakobj.dk>
2025-07-05 14:59:47 +0200

    tmp

--- make.d/clean.mk
+++ make.d/clean.mk
@@ -2,5 +2,5 @@ clean: _clean
 	docker compose down --remove-orphans --rmi local
 
 _clean:
-	-find build -not -name ".gitignore" -not -name ".gitkeep" -type f -delete
+	-find build -not -name ".gitignore" -and -not -name ".gitkeep" -type f -delete
 	-find build -type d -empty -delete
