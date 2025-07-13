6788231cbbd46ade6c36fc9a73ce730e76f0ad26
Jakob Jensen <jakobj@jakobj.dk>
2025-07-13 13:16:10 +0200

    tmp

--- test/verify-10-test.sh
+++ test/verify-10-test.sh
@@ -3,15 +3,15 @@ set -e
 echo "Verifing $1 ..."
 
 {
-  cd /schema || true
-  find . -type f -name "*.json"
-  cd - &>/dev/null || true
+    cd /schema || true
+    find . -type f -name "*.json"
+    cd - &>/dev/null || true
 } | sort > /tmp/schema.list
 
 {
-  cd /verified-schema || true
-  find . -type f -name "*.json"
-  cd - &>/dev/null || true
+    cd /verified-schema || true
+    find . -type f -name "*.json"
+    cd - &>/dev/null || true
 } | sort > /tmp/verified.list
 
 diff /tmp/schema.list /tmp/verified.list
