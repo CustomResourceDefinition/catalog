e7102342a5760a05c84255cc65a02ec3b217992a
Jakob Jensen <jakobj@jakobj.dk>
2025-07-20 16:14:42 +0200

    Avoid cleaning build/remote checkouts (#55)

--- make.d/clean.mk
+++ make.d/clean.mk
@@ -2,5 +2,5 @@ clean: _clean
 	docker compose down --remove-orphans --rmi local
 
 _clean:
-	-find build -not -name ".gitignore" -and -not -name ".gitkeep" -type f -delete
+	-find build -type f ! -name ".gitignore" ! -name ".gitkeep" ! -path "build/remote/*" ! -path "build/remote" -delete
 	-find build -type d -empty -delete
