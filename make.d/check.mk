154bda23871e19dec4064c45bf4b0b61e5d2ecc4
Jakob Jensen <jakobj@jakobj.dk>
2025-11-14 17:40:55 +0100

    chore: update sources to use less genPaths

--- make.d/check.mk
+++ make.d/check.mk
@@ -1,4 +1,5 @@
 _check:
 	-find build/ephemeral -not -name ".gitignore" -and -not -name ".gitkeep" -type f -delete
 	-find build/ephemeral -type d -empty -delete
-	build/bin/catalog update --configuration test/configuration.yaml --output build/ephemeral/schema --definitions build/ephemeral/schema
+	touch build/configuration.yaml
+	build/bin/catalog update --configuration build/configuration.yaml --output build/ephemeral/schema --definitions build/ephemeral/schema
