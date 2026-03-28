bdeb9056335bacb7b9335d87d51d14140d70d161
Jakob Jensen <jakobj@jakobj.dk>
2026-03-28 21:35:56 +0100

    tmp

--- make.d/update.mk
+++ make.d/update.mk
@@ -1,2 +1,7 @@
 update: build/bin/catalog
-	build/bin/catalog update --registry registry.yaml --configuration configuration.yaml --output schema --definitions definitions
+	build/bin/catalog update \
+		--registry registry.yaml \
+		--configuration configuration.yaml \
+		--output schema \
+		--definitions definitions \
+		--performance-log build/ephemeral/performance.log
